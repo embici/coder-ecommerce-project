1b533e239f6ffe44041637d3aacdf0dfad6ba658
Esteban Montero <estebanmontero@Estebans-MacBook-Air.local>
2022-06-21 08:28:38 -0600

    checking port

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, './build')));
 
+console.log('----> PORT', process.env.PORT);
 
 const mockResponse = {
   foo: 'bar',
